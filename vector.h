cb7a236bc40d131b4de291ae5742de661a741da4
t-e-mby <t.elkarif@mby.co.il>
2020-06-24 10:06:49 +0300

    several fixes

--- vector.h
+++ vector.h
@@ -9,54 +9,63 @@ class Vector
 {
 public:
     Vector();
-    Vector(size_t num, T val);
+    explicit Vector(size_t num, const T& val= T()); //init vector of num elements with val T
     Vector(const Vector&);
     ~Vector();
+
     Vector& operator=(const Vector<T>&);
-//capacity:
+    T& operator[](size_t index); //set
+    const T& operator[](size_t index)const; //get
+
     size_t size()const;
     size_t max_size()const;
-    void resize(size_t num, T val=T(0));
+    void resize(size_t num, T val=T());
     size_t capacity()const;
     bool empty()const;
     void reserve(size_t);
-//element access:
-    T& operator[](size_t index);
-    const T& operator[](size_t index)const;
     T& at(size_t index);
     T& front();
     const T& front()const ;
-    const T& back()const;
-    const T* data()const;
     T& back();
+    const T& back()const;
     T* data();
-//modifiers:
-    void assign(size_t n, const T& val);
+    const T* data()const;
+    void assign(size_t n, const T& val=T());
     void push_back(const T&);
     void pop_back();
     void insert (size_t position, size_t n, const T& val);
-    void erase (size_t position);
-    void erase (size_t first, size_t last);
+    void erase (size_t position, size_t num = 1); //todo erase number of elements form position on
     void swap(Vector& x);
     void clear();
+    friend std::ostream& operator <<(std::ostream&os, const Vector<T>&);
 
 private:
+    void init(size_t n);
     T* m_vector;
     size_t m_size;
     size_t m_capacity;
 };
 
 template<typename T>
-Vector<T>::Vector():m_vector(NULL),m_size(0),m_capacity(0) {}
+void Vector<T>::init(size_t n)
+{
+    T * tmp =new T[n];
+    if(m_vector!= NULL)
+        delete[] m_vector;
+    m_vector = tmp;
+}
 
 template<typename T>
-Vector<T>::Vector(size_t num, T val):m_vector(new T[num]),m_size(num),m_capacity(num) {
+Vector<T>::Vector():m_vector(init(1)),m_size(0),m_capacity(1) {}
+
+template<typename T>
+Vector<T>::Vector(size_t num, const T& val):m_vector(init(num)),m_size(num),m_capacity(num) {
     for (size_t i = 0; i <num ; i++) {
         m_vector[i]=val;
     }
 }
 template<typename T>
-Vector<T>::Vector(const Vector& other):m_vector(new T[other.m_capacity]),m_size(other.m_size),m_capacity(other.m_capacity){
+Vector<T>::Vector(const Vector& other):m_vector(init(other.m_capacity)),m_size(other.m_size),m_capacity(other.m_capacity){
     for (size_t i = 0; i <m_size ; ++i) {
         m_vector[i]=other.m_vector[i];
     }
@@ -65,13 +74,17 @@ Vector<T>::Vector(const Vector& other):m_vector(new T[other.m_capacity]),m_size(
 template<typename T>
 Vector<T>::~Vector() {
     delete [] m_vector;
+    m_vector= NULL;
+    m_size=0;
+    m_capacity=0;
 }
 
 template<typename T>
 Vector<T>& Vector<T>::operator=(const Vector<T>& other){
-    T* tmp=new T[other.m_capacity];
-    if(tmp)
+
+    if(this->m_vector!=other.m_vector)
     {
+        T* tmp=new T[other.m_capacity];
         for (size_t i = 0; i < other.m_size; ++i) {
             tmp[i]=other.m_vector[i];
         }
@@ -80,16 +93,17 @@ Vector<T>& Vector<T>::operator=(const Vector<T>& other){
         m_capacity=other.m_capacity;
         m_size=other.m_size;
     }
+
     return *this;
 }
 
 template<typename T>
-size_t Vector<T>::size()const{
+inline size_t Vector<T>::size()const{
     return m_size;
 }
 
 template<typename T>
-size_t Vector<T>::max_size()const{
+inline size_t Vector<T>::max_size()const{
     return SIZE_MAX;
 }
 
@@ -100,12 +114,12 @@ void Vector<T>::resize(size_t num, T val){
         for(size_t i =num; i<m_size;i++)
             m_vector[i].~T();
         m_size=num;
-        return;
     }
-    T* tmp=new T[num];
-    if(tmp)
+    else
     {
-        for (size_t i = 0; i < m_size,i<num; ++i) {
+        T* tmp=new T[num];
+
+        for (size_t i = 0; i < m_size; ++i) {
             tmp[i]=m_vector[i];
         }
         for (size_t j = m_size; j <num ; ++j) {
@@ -114,16 +128,17 @@ void Vector<T>::resize(size_t num, T val){
         delete[]m_vector;
         m_vector=tmp;
         m_size=num;
+        m_capacity = num;
     }
 }
 
 template<typename T>
-size_t Vector<T>::capacity()const{
+inline size_t Vector<T>::capacity()const{
     return m_capacity;
 }
 
 template<typename T>
-bool Vector<T>::empty()const{
+inline bool Vector<T>::empty()const{
     return m_size==0;
 }
 
@@ -136,18 +151,18 @@ void Vector<T>::reserve(size_t num){
 }
 
 template<typename T>
-T& Vector<T>::operator[](size_t index) {
+inline T& Vector<T>::operator[](size_t index) {
     return m_vector[index];
 }
 
 template<typename T>
-const T& Vector<T>::operator[](size_t index) const{
+inline const T& Vector<T>::operator[](size_t index) const{
     return m_vector[index];
 }
 
 
 template<typename T>
-T& Vector<T>::at(size_t index){
+inline T& Vector<T>::at(size_t index){
     if(index>m_size)
     {
         throw std::out_of_range("index out of range!");
@@ -156,34 +171,34 @@ T& Vector<T>::at(size_t index){
 }
 
 template<typename T>
-T& Vector<T>::front() {
+inline T& Vector<T>::front() {
     return m_vector[0];
 }
 
 template<typename T>
-const T& Vector<T>::front()const {
+inline const T& Vector<T>::front()const {
     return m_vector[0];
 }
 
 
 template<typename T>
-T& Vector<T>::back() {
+inline T& Vector<T>::back() {
     return m_vector[m_size-1];
 }
 
 template<typename T>
-const T& Vector<T>::back()const {
+inline const T& Vector<T>::back()const {
     return m_vector[m_size-1];
 }
 
 template<typename T>
-T* Vector<T>::data()
+inline T* Vector<T>::data()
 {
     return m_vector;
 }
 
 template<typename T>
-const T* Vector<T>::data()const
+inline const T* Vector<T>::data()const
 {
     return m_vector;
 }
@@ -196,20 +211,22 @@ void Vector<T>::assign(size_t n, const T& val){
         for (size_t i = 0; i<n; ++i) {
             m_vector[i]=val;
         }
+        for(size_t j=n; j<m_size;j++)
+        {
+            m_vector[j].~T();
+        }
         m_size=n;
         return;
     }
     T* tmp=new T[n];
-    if(*tmp!=NULL)
-    {
-        for (size_t i = 0; i<n; ++i) {
-            tmp[i]=val;
-        }
-        delete[]m_vector;
-        m_vector=tmp;
-        m_size=n;
-        m_capacity=n;
+
+    for (size_t i = 0; i<n; ++i) {
+        tmp[i]=val;
     }
+    delete[]m_vector;
+    m_vector=tmp;
+    m_size=n;
+    m_capacity=n;
 }
 
 template<typename T>
@@ -217,6 +234,8 @@ void Vector<T>::push_back(const T& val){
     if(m_size==m_capacity)
     {
         T* tmp=new T[m_capacity*2];
+        for(size_t i=0; i<m_size; i++)
+            tmp[i]=m_vector[i];
         m_vector = tmp;
         m_vector[m_size]= val;
         m_size++;
@@ -230,7 +249,7 @@ void Vector<T>::push_back(const T& val){
 }
 
 template<typename T>
-void Vector<T>::pop_back()
+inline void Vector<T>::pop_back()
 {
     ~m_vector[m_size-1];
     m_size--;
@@ -243,6 +262,10 @@ void Vector<T>::insert (size_t position, size_t n, const T& val)
     size_t size;
     if(m_capacity >= m_size+n)
     {
+        for(i=position+n; i<m_size+n; i++)
+        {
+            m_vector[i+n]= m_vector[i];
+        }
         for(i=0; i<n; i++)
         {
             m_vector[position+i+n] =  m_vector[position+i];
@@ -276,7 +299,7 @@ void Vector<T>:: erase (size_t position)
     m_vector[position].~T();
     for(i=position; i<m_size-1;i++)
         m_vector[i]= m_vector[i+1];
-    //last place guarbage???
+    m_size--;
 }
 
 template<typename T>
@@ -285,8 +308,48 @@ void Vector<T>::erase (size_t first, size_t last)
     size_t i;
     for(i=first; i<last;i++)
         m_vector[i].~T();
-    for(i=first; i<m_size-1;i++)
+    for(i=first; i<m_size-(last-first);i++)
         m_vector[i]= m_vector[i+last-first];
+    m_size-=last-first;
+}
+
+template<typename T>
+void Vector<T>::swap (Vector& x)
+{
+    T* tmp;
+    tmp= m_vector;
+    m_vector = x.m_vector;
+    x.m_vector = tmp;
+
+    size_t tmp1= m_size;
+    m_size=x.m_size;
+    x.m_size=tmp1;
+
+    size_t tmp2= m_capacity;
+    m_capacity=x.m_capacity;
+    x.m_capacity=tmp1;
+}
+
+template<typename T>
+void Vector<T> :: clear()
+{
+    size_t i;
+    for(i=0; i<m_size; i++)
+    {
+        m_vector[i].~T();
+    }
+    delete[](m_vector);
+    m_size= 0;
+    m_capacity = 0;
+}
+
+template<typename T>
+inline std::ostream& operator << (std::ostream&os, const Vector<T>& x)
+{
+    int *p = x.data();
+    for(size_t i=0; i<x.size(); i++)
+        os<<p[i]<<' ';
+    return os;
 }
 
 #endif
